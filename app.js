2f6c7cd34d3b120aaca704803388e66229f64b6c
Retsard <jshbangoy@gmail.com>
2023-05-16 00:40:14 +0800

    added 2 event listener

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const cameraBtn = document.getElementById("cameraBtn");
+const captureBtn = document.getElementById("captureBtn");
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
-const video = document.getElementById("video");
+const capturedImg = document.getElementById("capturedImg");
 
 cameraBtn.addEventListener("click", () => {
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
@@ -9,9 +10,23 @@ cameraBtn.addEventListener("click", () => {
         .then(function(stream) {
             video.srcObject = stream;
             video.play();
+            captureBtn.style.display = "block";
         })
         .catch(function(error) {
             console.error("Unable to access the camera", error);
         })
     }
+});
+
+captureBtn.addEventListener("touchstart", () => {
+    context.drawImage(video, 0, 0, canvas.clientWidth, canvas.height);
+    capturedImg.src = canvas.toDataURL();
+    capturedImg.style.display = "block";
+    video.style.display = 'none';
+    captureBtn.style.display = 'none';
+});
+
+window.addEventListener('resize', () => {
+    canvas.width = canvas.clientWidth;
+    canvas.height = canvas.clientHeight;
 });
